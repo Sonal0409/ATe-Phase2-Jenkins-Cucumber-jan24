cccc16b48bb2e8033dad1ebb905660107fcb696e
sonal0409 <mittal.sonal04@gmail.com>
2024-01-30 11:37:44 +0530

    done

---
+++ test-output-thread/report.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+CucumberHTML.timelineItems.pushArray([{"id":"c8bfeb06-2e1e-4664-8374-cb3ca03f3905","feature":"Register multiple users on the Rediff account Page","scenario":"Test rediff Register Account Page","start":1706593030036,"group":1,"content":"","tags":"","end":1706593056657,"className":"passed"}]);
+CucumberHTML.timelineGroups.pushArray([{"id":1,"content":"Thread[main,5,main]"}]);
+});
