3e384900db3c5d81fbe780d262be540c1e7249de
sonal0409 <mittal.sonal04@gmail.com>
2024-01-30 12:29:49 +0530

    done

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -9,14 +9,14 @@ import io.cucumber.junit.CucumberOptions;
 @CucumberOptions(
 		features=
 	"C:\\Users\\sonal\\Eclipse-2023\\ATE-Phase2-Cucumber-ExtentReports-Jenkins\\src\\test\\java\\features\\rediffRegister.feature",
-		glue = {"steps"} // packagename where the steps are
-		//plugin= {"pretty",
-		//		"html:target/cucumberreport.html",
-		//		"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
-		//		"timeline:test-output-thread/"
+		glue = {"steps"}, //packagename where the steps are
+		plugin= {"pretty",
+		"html:target/cucumberreport.html",
+		"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
+			"timeline:test-output-thread/"
 		
 		
-		//}
+		}
 		
 		)
 
